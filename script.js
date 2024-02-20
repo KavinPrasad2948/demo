45b175bb97fd97afcd410eec745115257ed41200
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-02-20 21:37:44 +0530

    Updated:script.js

--- script.js
+++ script.js
@@ -1,2 +1,51 @@
-console.log("hi this is kavin");
-console.log("hellow");
+
+
+  /* console.log(name,place,food);
+   for(let a=0;a<name.length||a<
+    place.length||a<food.length;a++){
+        console.log(a);}
+    // ? html for example
+    //! copy by reference
+    let vari ={ num:10};
+    let vari2={ num: 10};
+    let vari1= vari;
+    console.log(vari,vari1,vari2);
+    vari.num=20;
+    console.log(vari,vari1,vari2);
+    
+    //! copy by value
+
+    let num = 10;
+    let num1 = num;
+    console.log(num,num1);
+    num1 = 20;
+    console.log(num,num1);
+    
+   let a=[10,20,30,40,50,60];
+   console.log(a.length,a[0]);
+   a.unshift(0);
+   console.log(a.indexOf(20));
+   console.log(a.unshift(0),a);*/
+  /* let bio={
+    name:"Kavin",
+    place:"Sivagangai",
+    course:"full stack"
+   };
+   console.log(bio);
+   console.log(bio.name);
+   console.log(bio.course);
+   console.log(bio["place"]);
+   console.log(bio["username"]);
+   var i;
+   console.log(i);*/
+   /*let b=10
+   let n=9
+  if(b<n){
+
+   var a= 1;
+  }
+  console.log(a);   */
+  const req =new XMLHttpRequest();
+
+
+
